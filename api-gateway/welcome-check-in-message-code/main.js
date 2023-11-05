92c43a5c5a5bd417b582c95dc3c49279fb5e5d6c
Claudio Ramos de oliveira <claudior.oliveira@e-ai.com.br>
2023-11-05 20:43:46 -0300

    atualizando lambda

--- api-gateway/welcome-check-in-message-code/main.js
+++ api-gateway/welcome-check-in-message-code/main.js
@@ -1,11 +1,27 @@
-const jwt_decode = require('jwt-decode');
+const http = require("http");
+const url = "http://fastfood-ecs-1764329678.us-east-1.elb.amazonaws.com/customers/"
+
 
 exports.handler = async (event) => {
 
-    const tokenDecode = jwt_decode(event.headers.Authorization);
+    const { claims } = event.requestContext.authorizer
 
-    return  {
-        statusCode: 200,
-        body: JSON.stringify('Welcome to Royal ' + tokenDecode.name + "!"),
-    };
+
+    const response = await new Promise(function (resolve, reject) {
+
+        http.get(url + "45207947818", (res) => {
+
+            console.log(res.statusCode)
+
+            resolve(res.statusCode)
+
+        }).on("error", (e) => {
+            reject(Error(e));
+        });
+    });
+
+
+    return response;
+
+
 };
